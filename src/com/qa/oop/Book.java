d38bb1bcc16ba122830d8c7b5f0db940518eecde
Ali Guzel <ali.guzel@accenture.com>
2021-09-02 13:32:59 +0100

    Updated files with Encapsulation

--- src/com/qa/oop/Book.java
+++ src/com/qa/oop/Book.java
@@ -1,14 +1,18 @@
 package com.qa.oop;
 
 public class Book {
-	String title;
-	String blurb;
-	String author;
-	String publisher;
-	int year;
-	int pages;
+	private static int booksMade;
+	private String title;
+	private String blurb;
+	private String author;
+	private String publisher;
+	private int year;
+	private int pages;
+	private static String category = "Bestseller";
 	
 	public Book() {
+		super();
+		booksMade++;
 		this.title = "Harry Potter and the Chamber of Secrets";
 		this.blurb = "Harry Potter and the Chamber of Secrets begins when Harry is spending a miserable summer with his only remaining family, the Dursleys. During a dinner party hosted by his uncle and aunt, Harry is visited by Dobby, a house-elf.";
 		this.author = "J.K. Rowling";
@@ -23,6 +27,93 @@ public class Book {
 			System.out.println("Page: " + i);
 		}
 	}
+	
+	public static void staticMethod() {
+		System.out.println("this is a static method");
+	}
+
+	
+
+	public Book(String title, String blurb, String author, String publisher, int year, int pages) {
+		super();
+		booksMade++;
+		this.title = title;
+		this.blurb = blurb;
+		this.author = author;
+		this.publisher = publisher;
+		this.year = year;
+		this.pages = pages;
+	}
+
+	public static int getBooksMade() {
+		return booksMade;
+	}
+
+	public static void setBooksMade(int booksMade) {
+		Book.booksMade = booksMade;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBlurb() {
+		return blurb;
+	}
+
+	public void setBlurb(String blurb) {
+		this.blurb = blurb;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		if (year < 1990) {
+			System.out.println("Year cannot be less than 1990");
+		} else {
+			this.year = year;
+		}
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public static String getCategory() {
+		return category;
+	}
+
+	public static void setCategory(String category) {
+		Book.category = category;
+	}
+	
+	
 	
 	
 }
--- src/com/qa/oop/Runner.java
+++ src/com/qa/oop/Runner.java
@@ -4,8 +4,20 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Book harryPotter2 = new Book();
-		harryPotter2.readBook();
+		Book madeUpBook = new Book ("title", null, null, null, 0, 10);
+		System.out.println(Book.getBooksMade());
+		
+		System.out.println(harryPotter2.getCategory());
+		harryPotter2.setCategory("Not Bestseller");
+		System.out.println(harryPotter2.getCategory());
+		
+		System.out.println(madeUpBook.getCategory());
+		Book.setCategory("ALL RUBBISH");
+		System.out.println(madeUpBook.getCategory());
+		System.out.println(harryPotter2.getCategory());
 
+		Book.staticMethod();
+		madeUpBook.staticMethod();
 	}
 
 }
